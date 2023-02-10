54e9895a4f10bc72d6427b25438ab41b102f681d
JACCARD John <John.JACCARD@cpnv.ch>
2023-02-10 09:03:26 +0100

    display and new game fixed

--- Sprint_1/Projet1.0_début_affichage/2048_1.01.py
+++ Sprint_1/Projet1.0_début_affichage/2048_1.01.py
@@ -10,7 +10,10 @@ import tkinter.font
 import random
 
 # tableau 2 dimensions avec des mots (4x4)
-words = [[8192, 4096, 2048, 0],[1024, 512, 0, 256],[128, 0, 64, 32],[16, 8, 4, 2],]
+values_tables = [[8192, 4096, 2048, 0],
+                 [1024, 512, 0, 256],
+                 [128, 0, 64, 32],
+                 [16, 8, 4, 2]]
 
 # possible values
 values = [0,2,4]#,8,16,32,64,128,256,512,1024,2048,4096,8192]
@@ -22,7 +25,10 @@ colors = {0: '#ffffff', 2: '#FFE3CC', 4: '#DCC3A1', 8: '#03A678', 16: '#02735E',
 score = 0
 
 # tableau 2 dimensions avec des vides qui deviendront des labels.
-labels = [[None,None,None,None],[None,None,None,None],[None,None,None,None],[None,None,None,None],]
+labels = [[None,None,None,None],
+          [None,None,None,None],
+          [None,None,None,None],
+          [None,None,None,None]]
 
 width = 150 #espacement horizontal en pixels des étiquettes (remarque la taille des labels est en caractères)
 height = 150 #espacement vertical en pixels des étiquettes
@@ -42,36 +48,46 @@ fen.resizable(width=False, height=False)
 Ltitle = Label(text='2048', font=("Arial",40),bg='#0C343D',fg='white').pack(pady=10)
 
 #Création des labels (d'abord on les définit avec =, puis on les place dans la fenêtre avec .place(x,y)
-def displaystart(words):
-    for line in range(len(words)):
-        for col in range(len(words[line])):
-            # construction de chaque label sans le placer
-            # prendre la couleur selon la valeur de la case
-            var = words[line][col]
-            color = colors[var]
-            textcolor='white'
-            # changer la couleur de certaine cases pour améliorer la lisibilité
-            if var in [2,4,64]:
-                textcolor = 'black'
-            # création label
-            labels[line][col] = tkinter.Label (text =words[line][col], width=6, height=3, borderwidth=1, relief="solid", font=("Arial", 30), fg=textcolor, bg=color)
-            if var == 0:
-                labels[line][col].config(text="")
-            # placement du label dans la fenêtre par ses coordonnées en pixels
-            labels[line][col].place(x=30 + width * col, y=120 + height * line)
+for line in range(len(values_tables)):
+    for col in range(len(values_tables[line])):
+    # construction de chaque label sans le placer
+        # création label
+        labels[line][col] = tkinter.Label (text =values_tables[line][col], width=6, height=3, borderwidth=1, relief="solid", font=("Arial", 30))
 
-# Initialiser l'affichage du jeu
-displaystart(words)
+        # placement du label dans la fenêtre par ses coordonnées en pixels
+        labels[line][col].place(x=30 + width * col, y=120 + height * line)
 
 # function to randoms label
-def display():
-    words = [[random.choice(values), random.choice(values), random.choice(values), random.choice(values)],[random.choice(values), random.choice(values), random.choice(values), random.choice(values)],[random.choice(values), random.choice(values), random.choice(values), random.choice(values)],[random.choice(values), random.choice(values), random.choice(values), random.choice(values)],]
-       for line in range(len(words)):
-        for col in range(len(words[line])):
-            labels[line][col].config(text=words[line][col])
+def display(values_table):
+    for line in range(len(values_tables)):
+        for col in range(len(values_tables[line])):
+
+             # prendre la couleur selon la valeur de la case
+            var = values_tables[line][col]
+            color = colors[var]
+            textcolor = 'white'
+
+            # changer la couleur de certaine cases pour améliorer la lisibilité
+            if var in [2, 4, 64]:
+                textcolor = 'black'
+
+            # modifier le text et la couleur des cases
+            labels[line][col].config(text=values_tables[line][col],bg=color,fg=textcolor)
+            if var == 0:
+                labels[line][col].config(text="")
+
+display(values_tables)
+
+def newGame():
+    global values_tables
+    values_tables = [[random.choice(values), random.choice(values), random.choice(values), random.choice(values)],
+                     [random.choice(values), random.choice(values), random.choice(values), random.choice(values)],
+                     [random.choice(values), random.choice(values), random.choice(values), random.choice(values)],
+                     [random.choice(values), random.choice(values), random.choice(values), random.choice(values)]]
+    display(values_tables)
 
 # recommencer le jeu
-Brestart = Button(text='New game',bg='#0C343D',fg='white',borderwidth=0, command=display, activebackground='#0C343D').pack()
+Brestart = Button(text='New game',bg='#0C343D',fg='white',borderwidth=0, command=newGame, activebackground='#0C343D').pack()
 
 
 
