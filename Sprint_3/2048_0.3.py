a615870650d58acef29f27fa97fcb24de440b8e8
John.JACCARD <Jackofswiss@gmail.com>
2023-03-17 11:13:39 +0100

    Sprint 3 avec debug mode

--- Sprint_3/2048_0.3.py
+++ Sprint_3/2048_0.3.py
@@ -12,18 +12,17 @@ import copy
 import json
 from tkinter import messagebox
 
+blank_table = [[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]]
+
 
 # tableau 2 dimensions avec des valeurs (4x4)
-values_tables = [[0, 0, 0, 0],
-                 [0, 0, 0, 0],
-                 [0, 0, 0, 0],
-                 [0, 0, 0, 0]]
+values_tables = copy.deepcopy(blank_table)
 
 # save of vaules table
 table = 0
 
-# initiate nmove
-nmove = 0
+# set undo table value
+undo_table = copy.deepcopy(values_tables)
 
 # Faire
 end_game = False
@@ -32,73 +31,66 @@ end_game2 = False
 # liste chiffre apparaisable
 value_list = [2, 2, 2, 2, 4]
 
-# initiate highscore
+# Prendre le score highscore
 text = open("HighScore.txt", "r")
-Highscore = text.read()
+Highscore = int(text.read())
 text.close()
 
-# # fonction pour trier les 0 afin de tasser plus facilement
-def move(a, b, c, d):
-    global nmove
+# # fonction pour trier les 0 et les mettre les 0 à la fin et pouvoir tasser proprement
+def sort(a, b, c, d):
     if a == 0 and b != 0:
         a, b, c, d = b, c, d, a
-        nmove += 1
 
     if b == 0 and c != 0:
         b, c, d = c, d, b
-        nmove += 1
 
     if c == 0 and d != 0:
         c, d = d, c
-        nmove += 1
 
     if a == 0 and b != 0:
         a, b, c, d = b, c, d, a
-        nmove += 1
 
     if b == 0 and c != 0:
         b, c, d = c, d, b
-        nmove += 1
 
     return a, b, c, d
 
 # fonction pour tasser (aide de léo)
 def tasse_4(a,b,c,d):
-    global score, nmove, Highscore
-    a, b, c, d = move(a, b, c, d)
-    # tassage
+    global score
+    a, b, c, d = sort(a, b, c, d)
+    # tassage si a et b sont identiques il va les tasser et ajouter ce tassement au score
     if a == b:
         a = a*2
         b = 0
         score += a
-        nmove += 1
 
+    # tassage si b et c sont identiques il va les tasser et ajouter ce tassement au score
     if b == c:
         b = b*2
         c = 0
         score += b
-        nmove += 1
 
+    # tassage si c et d sont identiques il va les tasser et ajouter ce tassement au score
     if c == d:
         c = c*2
         d = 0
         score += c
-        nmove += 1
 
-    if int(score) >= int(Highscore):
-        Highscore = score
-        text = open("HighScore.txt", "w")
-        text.write(str(Highscore))
-        text.close()
-
-    a, b, c, d = move(a, b, c, d)
+    a, b, c, d = sort(a, b, c, d)
     return [a, b, c, d]
 
 
 def tass_bind(event):
-    global table
+    global table,undo_table,values_tables,blank_table,undo_score
     # Copie de la table de base
     save_table = copy.deepcopy(values_tables)
+
+    # Copie de la table de base pour le bouton Undo
+    undo_table = copy.deepcopy(values_tables)
+
+    # Copie du score pour le bouton Undo
+    undo_score = copy.deepcopy(score)
 
     # Assigner la touche à une variable
     Key = event.keysym
@@ -155,7 +147,7 @@ def tass_bind(event):
     game.write(str(values_tables))
     game.close()
 
-    # Prends le score de la game precedemment quittée
+    # Écrit le score de la partie dans un fichier texte pour que quand on quitte la partie en revenant nous aurons toujours le même score
     score_saving = open("Score.txt", 'w')
     score_saving.write(str(score))
     score_saving.close()
@@ -170,6 +162,7 @@ colors = {0: '#ffffff', 2: '#FFE3CC', 4: '#DCC3A1', 8: '#03A678', 16: '#02735E',
 score_base = open("Score.txt", 'r')
 try:
     score = int(score_base.read())
+    undo_score = score
 except ValueError:
     score = 0
 score_base.close()
@@ -208,25 +201,35 @@ for line in range(len(values_tables)):
         # Placement du label dans la fenêtre par ses coordonnées en pixels
         labels[line][col].place(x=30 + width * col, y=120 + height * line)
 
-
 # Assigner la fonction tass aux touches
 fen.bind("<Key>", lambda event: tass_bind(event))
 
 # Score du jeu
 Lscore = Label(font=("Arial", 17), bg='#0C343D', fg='white')
-LHighScore = Label(font=("Arial", 17),bg='#0C343D', fg='white',text=f'Highscore : {Highscore}')
+LHighScore = Label(font=("Arial", 17),bg='#0C343D', fg='white', text=f'Highscore : {Highscore}')
+
+# Touche afin d'annuler la dernière action
+BUndo_image = PhotoImage(file='Undo_img.png')
+BUndo = Button( bg='#0C343D', activebackground='#0C343D', borderwidth=0,image=BUndo_image,command=lambda: Undo())
 
 # Image et écran de victoire + le bouton continuer sur l'écran de victoire
 win_screen = PhotoImage(file='win_screen.png',width=600,height=600)
 Lwin_screen = Label(image=win_screen,bg='white')
-Bwin = Button(bg='white',fg='black',borderwidth=0, activebackground='white',text="Continue",command=lambda:continue_game(),font=("Ubuntu Mono", 30, "italic"))
+Bwin = Button(bg='white',fg='black',borderwidth=0, activebackground='white',text="Continue",command=lambda: continue_game(),font=("Ubuntu Mono", 30, "italic"))
 
 # Image et écran de défaite + le bouton continuer sur l'écran de défaite
 loose_screen = PhotoImage(file='loose_screen.png',width=600,height=600)
 Lloose_screen = Label(image=loose_screen,bg='grey')
-Bloose = Button(bg='grey',fg='white',borderwidth=0, activebackground='grey',text="Afficher le jeu",command=lambda:loose_game(),font=("Ubuntu Mono", 30, "italic"))
+Bloose = Button(bg='grey',fg='white',borderwidth=0, activebackground='grey',text="Afficher le jeu",command=lambda : loose_game(),font=("Ubuntu Mono", 30, "italic"))
 
-# Fonction qui affiche le menu de spawn
+# Fonction afin de revenir une action en arrière dans le jeu
+def Undo():
+    global values_tables,undo_table,undo_score,score
+    values_tables = undo_table
+    score = undo_score
+    display(values_tables)
+
+# Fonction qui affiche le menu debug
 def cheat_screen():
     sizex = 900
     sizey = 800
@@ -239,8 +242,10 @@ def cheat_screen():
     Echeat_nb.place(x=725,y=160)
     Bcheat_confirm.place(x=730,y=180)
     Bcheat_randomnb.place(x=730,y=220)
+    Bcheat_randomcolor.place(x=710,y=250)
+    Bclear_highscore.place(x=715,y=300)
 
-# Fonction pour quitter le menu de spawn
+# Fonction pour quitter le menu debug
 def quit_cheat_screen():
     sizex = 650
     sizey = 800
@@ -265,6 +270,42 @@ def confirm_cheat_screen():
         messagebox.showerror("Erreur", "Veuillez saisir un nombre valide")
     display(values_tables)
 
+# Fonction afin de convertir de code rgb en hexa la couleur
+def rgb_to_hexa(rgb):
+    return "#%02x%02x%02x" % rgb
+
+# Fonction afin d'arrêter les couleur multicolore et remettre ensuite la fonction de base
+def randomcoloroff():
+    global Random_color,Bcheat_randomcolor
+    Random_color = 0
+    Bcheat_randomcolor.config(command=lambda: randomcolorcheck())
+
+# Cette fonction génère trois valeur de 0 a 255 pour chaque case du tableau et les appliquent
+# A la fin il change le bouton afin que en réappuyant on arrête le mode rainbow
+def cheat_randomcolor_screen():
+    global values_tables,Ccheat_randomcolor,Random_color
+    for line in range(len(values_tables)):
+        for col in range(len(values_tables[line])):
+            r = random.randint(0, 255)
+            g = random.randint(0, 255)
+            b = random.randint(0, 255)
+            rgb = (r, g, b)
+            rgb = rgb_to_hexa(rgb)
+            labels[line][col].config(text=values_tables[line][col], bg=rgb)
+            if values_tables[line][col] == 0:
+                labels[line][col].config(text="")
+    Bcheat_randomcolor.config(command=lambda: randomcoloroff())
+
+# Lance en boucle la fonction randomcolorcheck avec un temps entre chaque fois et sinon le réactive pour le prochain appuie
+def randomcolorcheck():
+    global Random_color
+    if Random_color == 1:
+        cheat_randomcolor_screen()
+        fen.after(4, randomcolorcheck)
+    else:
+        Random_color = 1
+
+
 # Fonction permettant de random la position de tout les tuiles avec une valeur
 def random_cheat_screen():
     global values_tables
@@ -272,10 +313,7 @@ def random_cheat_screen():
     for i in values_tables:
         for y in i:
             values.append(y)
-    values_tables = [[0, 0, 0, 0],
-                     [0, 0, 0, 0],
-                     [0, 0, 0, 0],
-                     [0, 0, 0, 0]]
+    values_tables = copy.deepcopy(blank_table)
     for j in values:
         x = random.randint(0, 3)
         y = random.randint(0, 3)
@@ -285,6 +323,10 @@ def random_cheat_screen():
         values_tables[x][y] = j
     display(values_tables)
 
+def clear_highscore():
+    global Highscore
+    Highscore = 0
+    display(values_tables)
 
 # Bouton permettant dâfficher le menu de spawn de tuile
 Bcheat = Button(text='DEBUG',bg='#0C343D',fg='#0C343D',borderwidth=0, command=lambda: cheat_screen())
@@ -302,11 +344,20 @@ Lcheat_nb = Label(text='Veuillez insérer le nb à placer',bg='#0C343D',fg='whit
 Echeat_nb = Entry(width=10)
 
 # Bouton afin de confirmer le nombre et les coordonnées rentrées en vérifiant si elles sont correctes et afficher la case
-Bcheat_confirm = Button(text='Confirm',bg='#0C343D',fg='white',borderwidth=0, command=lambda:confirm_cheat_screen())
+Bcheat_confirm = Button(text='Confirm', bg='#0C343D', fg='white',borderwidth=0, command=lambda: confirm_cheat_screen())
 
 # Bouton relié à une fonction qui permet de random tout les nombres existant et les replacer
-Bcheat_randomnb = Button(text='Random',bg='#0C343D',fg='white',borderwidth= 0,command=lambda: random_cheat_screen())
+Bcheat_randomnb = Button(text='Random', bg='#0C343D', fg='white',borderwidth=0, command=lambda: random_cheat_screen())
 
+# Variable permettant de savoir si la fonction rainbow mod est activée ou non afin que au premier appuye le mode s'active et au deuxième il s'arrête
+Random_color = 1
+
+# Bouton permettant de lancer et d'arrêter le mode 'rainbow' avec une image
+Rainbow_img = PhotoImage(file='rainbow_mod_button.png',width=95,height=50)
+Bcheat_randomcolor = Button(image=Rainbow_img, bg='#0C343D', activebackground='#0C343D', borderwidth=0, command=lambda: randomcolorcheck())
+
+# Bouton permettant de remttre le highscore à 0
+Bclear_highscore = Button(text='Clear Highscore', bg='#0C343D', fg='white',borderwidth=0,command=lambda: clear_highscore())
 
 # Fonction afin de continuer le jeu quand on atteint 2048
 def continue_game():
@@ -327,7 +378,6 @@ def loose_game():
     Bloose = Button(bg='grey', fg='black', borderwidth=0, activebackground='grey', text="Afficher le jeu", command=lambda:loose_game(), font=("Ubuntu Mono", 30, "italic"))
     end_game2 = True
     return end_game2
-
 
 # Fonction pour check si il y a encore un mouvement possible
 def check():
@@ -372,13 +422,20 @@ def check():
 
 # Fonction afin de mettre les labels à la bonne couleur par rapport à leur couleur est les réafficher si la table à changée
 def display(values_table):
-    global score
+    global score,Highscore
+    # Augmentation du "Highscore" si le score dépasse le "Highscore"
+    if Highscore < score:
+        Highscore = score
+        text = open("HighScore.txt", "w")
+        text.write(str(Highscore))
+        text.close()
+
     for line in range(len(values_tables)):
         for col in range(len(values_tables[line])):
 
             # Prendre la couleur selon la valeur de la case
             var = values_tables[line][col]
-            if var>8192:
+            if var > 8192:
                 color = 'black'
             else:
                 color = colors[var]
@@ -404,10 +461,9 @@ def display(values_table):
 # Premier affichage de la table
 display(values_tables)
 
-
 # Fonction new game (aide de Thibault)
 def newGame():
-    global values_tables, score, end_game, end_game2
+    global values_tables, score, end_game, end_game2, undo_score
 
     # Enlever l'écran de victoire (si il a appuyé sur new game avec l'écran de win afficher)
     continue_game()
@@ -418,6 +474,7 @@ def newGame():
     end_game2 = False
 
     # Réinitisaliser le score
+    undo_score = copy.deepcopy(score)
     score = 0
 
     # Initialiser un tableau vide
@@ -446,16 +503,19 @@ Start_game = open("Gamesave.txt", 'r')
 if Start_game.read() != "":
     with open('Gamesave.txt', 'r') as temp_op:
         values_tables = json.load(temp_op)
+        undo_table = copy.deepcopy(values_tables)
     display(values_tables)
 # Sinon recommencer une nouvelle partie
 else:
     newGame()
 
-text.close()
+Start_game.close()
 
 # Recommencer le jeu
 Brestart = Button(text='New game',bg='#0C343D',fg='white',borderwidth=0, command=newGame, activebackground='#0C343D').pack()
 
+
+BUndo.place(x=30, y=30)
 Lscore.pack(side=BOTTOM)
 LHighScore.pack(side=BOTTOM)
 Bcheat.place(x=605, y=780)
